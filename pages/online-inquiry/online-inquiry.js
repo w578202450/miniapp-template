3fdd8b682705d7deb5e65d63d4cafa0b3c57699b
fu zhimin <15184467065@163.com>
2019-12-30 21:16:58 +0800

    首页

--- pages/online-inquiry/online-inquiry.js
+++ pages/online-inquiry/online-inquiry.js
@@ -4,51 +4,12 @@ const app = getApp()
 
 Page({
   data: {
-    motto: '',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    roomname: "付志敏的互联网诊所",
+    roomdes: "网上咨询·在线开药·预约就诊",
+    qualificationDes: "12313456DFGHJJJKN",
+    licensingDes: "12313456DFGHJJJKN",
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
   }
 })
