6b4c4971bb96db547f80ed63bb73687e1904cdab
baohexin <1191972217@qq.com>
2020-01-03 11:37:14 +0800

    修改bug

--- pages/online-inquiry/inquiry/index/index.js
+++ pages/online-inquiry/inquiry/index/index.js
@@ -1,6 +1,6 @@
-var util = require('../../utils/util.js'); // 转换时间插件
-var im = require('../../utils/webim_wx.js'); // 腾讯云 im 包
-var imhandler = require('../../utils/im_handler.js'); // 这个是所有 im 事件的 js
+var util = require('../../../../utils/util.js'); // 转换时间插件
+var im = require('../../../../utils/webim_wx.js'); // 腾讯云 im 包
+var imhandler = require('../../../../utils/im_handler.js'); // 这个是所有 im 事件的 js
 
 // 获取应用实例
 const app = getApp()
