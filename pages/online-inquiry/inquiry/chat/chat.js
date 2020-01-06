47d309dc3365706da94353a2f295571a5fc90c33
huangtao <huangtao>
2020-01-06 14:12:09 +0800

    gengx

--- pages/online-inquiry/inquiry/chat/chat.js
+++ pages/online-inquiry/inquiry/chat/chat.js
@@ -12,7 +12,9 @@ Page({
 
   data: {
     toView: 'green',
+    // 用户信息
     userInfo: { keyID: "" },
+    // 对话信息
     talkInfo: {
       patientInfo: {
         keyID: "111"
@@ -24,15 +26,18 @@ Page({
         keyID: "333"
       }
     },
+    // 聊天列表信息
     currentMessageList: [
-      { keyID: "1", type: "TIM.TYPES.MSG_TEXT", personID: "111", personName: "大娃", addTime: "2019-12-28 10:03:00", faceImage: "../../../../images/home/home_doctor.png", msgText: "医生，你好，我生病了。" },
-      { keyID: "2", type: "TIM.TYPES.MSG_TEXT", personID: "222", personName: "胡助理", addTime: "2019-12-28 10:03:00", faceImage: "../../../../images/home/home_doctor.png", msgText: "什么病？" },
-      { keyID: "3", type: "TIM.TYPES.MSG_TEXT", personID: "333", personName: "路医生", addTime: "2019-12-28 10:03:00", faceImage: "../../../../images/home/home_doctor.png", msgText: "请描述的详细一些，方便我们诊断。请描述的详细一些，方便我们诊断。请描述的详细一些，方便我们诊断。" },
-      { keyID: "4", type: "TIM.TYPES.MSG_TEXT", personID: "111", personName: "大娃", addTime: "2019-12-28 10:03:00", faceImage: "../../../../images/home/home_doctor.png", msgText: "脑壳痛，胸口闷，我有一点昏。脚杆青痛感觉不对头。" },
-      { keyID: "5", type: "TIM.TYPES.MSG_TEXT", personID: "111", personName: "大娃", addTime: "2019-12-28 10:03:00", faceImage: "../../../../images/home/home_doctor.png", msgText: "脑壳痛，胸口闷，我有一点昏。脚杆青痛感觉不对头。" },
-      { keyID: "6", type: "TIM.TYPES.MSG_IMAGE", personID: "111", personName: "大娃", addTime: "2019-12-28 10:03:00", faceImage: "../../../../images/home/home_doctor.png", msgText: "", imgInfo: { imgUrl: "../../../../images/home/home_doctor.png" } },
-      { keyID: "7", type: "TIM.TYPES.MSG_AUDIO", personID: "111", personName: "大娃", addTime: "2019-12-28 10:03:00", faceImage: "../../../../images/home/home_doctor.png", msgText: "", payload: { second: 5 } }
-    ]
+      // { keyID: "1", type: "TIM.TYPES.MSG_TEXT", personID: "111", personName: "大娃", addTime: "2019-12-28 10:03:00", faceImage: "../../../../images/home/home_doctor.png", msgText: "医生，你好，我生病了。" },
+      // { keyID: "2", type: "TIM.TYPES.MSG_TEXT", personID: "222", personName: "胡助理", addTime: "2019-12-28 10:03:00", faceImage: "../../../../images/home/home_doctor.png", msgText: "什么病？" },
+      // { keyID: "3", type: "TIM.TYPES.MSG_TEXT", personID: "333", personName: "路医生", addTime: "2019-12-28 10:03:00", faceImage: "../../../../images/home/home_doctor.png", msgText: "请描述的详细一些，方便我们诊断。请描述的详细一些，方便我们诊断。请描述的详细一些，方便我们诊断。" },
+      // { keyID: "4", type: "TIM.TYPES.MSG_TEXT", personID: "111", personName: "大娃", addTime: "2019-12-28 10:03:00", faceImage: "../../../../images/home/home_doctor.png", msgText: "脑壳痛，胸口闷，我有一点昏。脚杆青痛感觉不对头。" },
+      // { keyID: "5", type: "TIM.TYPES.MSG_TEXT", personID: "111", personName: "大娃", addTime: "2019-12-28 10:03:00", faceImage: "../../../../images/home/home_doctor.png", msgText: "脑壳痛，胸口闷，我有一点昏。脚杆青痛感觉不对头。" },
+      // { keyID: "6", type: "TIM.TYPES.MSG_IMAGE", personID: "111", personName: "大娃", addTime: "2019-12-28 10:03:00", faceImage: "../../../../images/home/home_doctor.png", msgText: "", imgInfo: { imgUrl: "../../../../images/home/home_doctor.png" } },
+      // { keyID: "7", type: "TIM.TYPES.MSG_AUDIO", personID: "111", personName: "大娃", addTime: "2019-12-28 10:03:00", faceImage: "../../../../images/home/home_doctor.png", msgText: "", payload: { second: 5 } }
+    ],
+    // 输入的聊天内容
+    maySendContent: ""
   },
 
   upper(e) {
@@ -71,6 +76,23 @@ Page({
     })
   },
 
+  /*输入预发送信息 */
+  adInputChange: function (e) {
+    let that = this;
+    // if (e.detail.value.length < 1) {
+    //   this.maySendContent = "";
+    // } else {
+    //   // that.setData({
+    //   //   adTitle: e.detail.value,
+    //   // })
+    //   this.maySendContent = e.detail.value;
+    // }
+     that.setData({
+       maySendContent: e.detail.value,
+      })
+    console.log(that.maySendContent);
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -89,7 +111,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
