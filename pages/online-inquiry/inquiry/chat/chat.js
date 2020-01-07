9df49cbf07e8da51e294b75fa4dbfbfbb6f0d94b
baohexin <1191972217@qq.com>
2020-01-07 10:47:14 +0800

    发送图片、语音消息

--- pages/online-inquiry/inquiry/chat/chat.js
+++ pages/online-inquiry/inquiry/chat/chat.js
@@ -151,7 +151,7 @@ Page({
     }
     // 开启隐性加载过程
     that.httpLoading = true;
-    // 发送文本消息，Web 端与小程序端相同
+    //------------------------------发送文本消息------------------------------
     // 1. 创建消息实例，接口返回的实例可以上屏
     let message = tim.createTextMessage({
       to: 'user1',
@@ -175,8 +175,82 @@ Page({
       that.getMsgListFun();
     }).catch(function(imError) {
       // 发送失败
-      console.warn("===发送失败===" + 'sendMessage error:', imError);
+      // console.warn("===发送失败===" + 'sendMessage error:', imError);
     });
+   //------------------------------发送文本消息------------------------------
+
+   //------------------------------发送图片消息------------------------------
+    // 小程序端发送图片
+    // 1. 选择图片
+    wx.chooseImage({
+      sourceType: ['album'], // 从相册选择
+      count: 1, // 只选一张，目前 SDK 不支持一次发送多张图片
+      success: function (res) {
+        // 2. 创建消息实例，接口返回的实例可以上屏
+        let message = tim.createImageMessage({
+          to: 'user1',
+          conversationType: TIM.TYPES.CONV_C2C,
+          payload: { file: res },
+          onProgress: function (event) { console.log('file uploading:', event) }
+        });
+        // 3. 发送图片
+        let promise = tim.sendMessage(message);
+        promise.then(function (imResponse) {
+          // 发送成功
+          console.log(imResponse);
+        }).catch(function (imError) {
+          // 发送失败
+          console.warn('sendMessage error:', imError);
+        });
+      }
+    });
+  //------------------------------发送图片消息------------------------------
+
+  //------------------------------发送语音消息------------------------------
+    // 示例：使用微信官方的 RecorderManager 进行录音，参考 RecorderManager.start(Object object)
+    // 1. 获取全局唯一的录音管理器 RecorderManager
+    const recorderManager = wx.getRecorderManager();
+
+    // 录音部分参数
+    const recordOptions = {
+      duration: 60000, // 录音的时长，单位 ms，最大值 600000（10 分钟）
+      sampleRate: 44100, // 采样率
+      numberOfChannels: 1, // 录音通道数
+      encodeBitRate: 192000, // 编码码率
+      format: 'aac' // 音频格式，选择此格式创建的音频消息，可以在即时通信 IM 全平台（Android、iOS、微信小程序和 Web）互通
+    };
+
+    // 2.1 监听录音错误事件
+    recorderManager.onError(function (errMsg) {
+      console.warn('recorder error:', errMsg);
+    });
+    // 2.2 监听录音结束事件，录音结束后，调用 createAudioMessage 创建音频消息实例
+    recorderManager.onStop(function (res) {
+      console.log('recorder stop', res);
+
+      // 4. 创建消息实例，接口返回的实例可以上屏
+      const message = tim.createAudioMessage({
+        to: 'user1',
+        conversationType: TIM.TYPES.CONV_C2C,
+        payload: {
+          file: res
+        }
+      });
+
+      // 5. 发送消息
+      let promise = tim.sendMessage(message);
+      promise.then(function (imResponse) {
+        // 发送成功
+        console.log(imResponse);
+      }).catch(function (imError) {
+        // 发送失败
+        console.warn('sendMessage error:', imError);
+      });
+    });
+
+    // 3. 开始录音
+    recorderManager.start(recordOptions);
+  //------------------------------发送语音消息------------------------------
   },
 
   /*打开、关闭 底部工具栏 */
