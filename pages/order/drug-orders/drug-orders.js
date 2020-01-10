f043059d89f1c42ec07f306dd4c040838132c2ec
fu zhimin <15184467065@163.com>
2020-01-10 16:14:18 +0800

    我的订单

--- pages/order/drug-orders/drug-orders.js
+++ pages/order/drug-orders/drug-orders.js
@@ -1,73 +1,79 @@
 const HTTP = require('../../../utils/http-util')
 
 Page({
+  noNetwork: false,
+  noData: false,
   data: {
-    list: [
-      {
-        avatar: '../../../images/public/public_avatar.png',
-        title: '李荣豪医生的治疗方案',
-        details: '症状：体弱多病',
-        date: '开方日期：2019-10-22',
-        status:'待支付',
-        button: 1,
-        id:0
-      },
-      {
-        avatar: '../../../images/public/public_avatar.png',
-        title: '李荣豪医生的治疗方案',
-        details: '症状：体弱多病，四肢酸软，腰痛1年多。',
-        date: '开方日期：2019-10-22',
-        status: '待发货',
-        button: 2,
-        id: 1
-      },
-      {
-        avatar: '../../../images/public/public_avatar.png',
-        title: '李荣豪医生的治疗方案',
-        details: '症状：体弱多病，四肢酸软，腰痛1年多。',
-        date: '开方日期：2019-10-22',
-        status: '已支付',
-        button: 3,
-        id: 2
-      },
-      {
-        avatar: '../../../images/public/public_avatar.png',
-        title: '李荣豪医生的治疗方案',
-        details: '症状：体弱多病，四肢酸软，腰痛1年多。',
-        date: '开方日期：2019-10-22',
-        status: '待发货',
-        button: 2,
-        id: 3
-      },
-      {
-        avatar: '../../../images/public/public_avatar.png',
-        title: '李荣豪医生的治疗方案',
-        details: '症状：体弱多病，四肢酸软，腰痛1年多。',
-        date: '开方日期：2019-10-22',
-        status: '已支付',
-        button: 3,
-        id: 4
-      },
-    ]
+    list: []
   },
   onLoad: function () {
+    this.loadDatas()
+  },
+
+  // 加载数据
+  loadDatas() {
     wx.showLoading({
-      title: '加载中...',
-      icon:'none'
-    })
+      title: '加载订单列表...',
+    });
+    this.data.list = [];
+    var that = this;
     HTTP.getOrderByPerson({
-      buyerID:'123',
-      orgID:'19122116554357936820511001'
+      buyerID: '123',
+      orgID: '19122116554357936820511001',
+      pageIndex: 1,
+      pageSize: 100
     })
       .then(res => {
         wx.hideLoading();
-        
+        if (res.code == 0) {
+          if (res.data.datas.length == 0) {
+            that.setData({
+              noData: true
+            })
+          } else {
+            this.data.list = this.data.list.concat(res.data.datas)
+          
+            var tempRpIds = []
+            for (var index in this.data.list) {
+              tempRpIds.push(this.data.list[index].rpID)
+            }
+
+            that.getRpByList({
+              orgID: this.data.list[0].orgID,
+              rpIDs: tempRpIds
+            })
+          }
+        }
       }).catch(e => {
         wx.hideLoading();
-        wx.showToast({
-          title: '获取信息失败',
-          icon: 'none',
-          duration: 2000
+        that.setData({
+          noNetwork: true
+        })
+      })
+  },
+  getRpByList(params){
+    wx.showLoading({
+      title: '加载处方详情...',
+    });
+    HTTP.getRpByList(params)
+      .then(res => {
+        wx.hideLoading();
+        if (res.code == 0) {
+          if (res.data.length > 0) {
+            for (var i in res.data){
+              this.data.list[i].diagnosis = res.data[i].diagnosis
+              this.data.list[i].doctorName = res.data[i].doctorName
+              console.log('diagnosis------', res.data.diagnosis)
+            }
+          } 
+        }
+        this.setData({
+          list: this.data.list
+        })
+      }).catch(e => {
+        wx.hideLoading();
+        that.setData({
+          noNetwork: true
         })
       })
   },
@@ -84,6 +90,20 @@ Page({
   orderDetailsAction:function(){
     wx.navigateTo({
       url: '/pages/order/order-details/order-details',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+
+  // 无网络
+  noNetworkOption: function () {
+    this.loadDatas()
+  },
+  // 无数据
+  noDataOption: function (e) {
+    wx.navigateTo({
+      url: '../../online-inquiry/inquiry/chat/chat',
       success: function (res) { },
       fail: function (res) { },
       complete: function (res) { },
