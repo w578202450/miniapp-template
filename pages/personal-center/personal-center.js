31588af7e5e5e6cc0db9bbf23ad0bc5b7480047d
fu zhimin <15184467065@163.com>
2020-01-10 11:35:03 +0800

    no message

--- pages/personal-center/personal-center.js
+++ pages/personal-center/personal-center.js
@@ -5,6 +5,8 @@ const app = getApp()
 Page({
   data: {
     userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     list: [{
         url: 'health-information/health-information',
         title: '我的健康信息'
@@ -27,14 +29,16 @@ Page({
   onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
-        userInfo: app.globalData.userInfo
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
       })
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
-          userInfo: res.userInfo
+          userInfo: res.userInfo,
+          hasUserInfo: true
         })
       }
     } else {
@@ -43,19 +47,21 @@ Page({
         success: res => {
           app.globalData.userInfo = res.userInfo
           this.setData({
-            userInfo: res.userInfo
+            userInfo: res.userInfo,
+            hasUserInfo: true
           })
         }
       })
     }
   },
 
-  fetchAuthAction:function(){
-    wx.navigateTo({
-      url: '/pages/tmc-tips/tmc-tips',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
+  getUserInfo: function (e) {
+    console.log(e)
+    console.log('-----------', e.detail.userInfo)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
     })
   }
 })
