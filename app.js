8d31098c4717a2938e84b9b95a7b8b9fd0336cfd
baohexin <1191972217@qq.com>
2020-01-14 09:13:55 +0800

    修改app.js

--- app.js
+++ app.js
@@ -28,33 +28,33 @@ tim.registerPlugin({
 
 App({
   onLaunch: function() {
-    // AUTH.wxlogin();
+      // AUTH.wxlogin();
 
-    // wx.getSetting({
-    //   success: res => {
-    //     console.log('-------授权成功')
-    //     if (res.authSetting['scope.userInfo']) {
-    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-    //       wx.getUserInfo({
-    //         success: res => {
-    //           // 可以将 res 发送给后台解码出 unionId
-    //           this.globalData.userInfo = res.userInfo
+      // wx.getSetting({
+      //   success: res => {
+      //     console.log('-------授权成功')
+      //     if (res.authSetting['scope.userInfo']) {
+      //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+      //       wx.getUserInfo({
+      //         success: res => {
+      //           // 可以将 res 发送给后台解码出 unionId
+      //           this.globalData.userInfo = res.userInfo
 
-    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //           // 所以此处加入 callback 以防止这种情况
-    //           if (this.userInfoReadyCallback) {
-    //             this.userInfoReadyCallback(res)
-    //           }
-    //         }
-    //       })
-    //     }
-    //   },
-    //   fail:res =>{
-    //     console.log('-------授权失败')
-    //   }
-    // })
+      //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      //           // 所以此处加入 callback 以防止这种情况
+      //           if (this.userInfoReadyCallback) {
+      //             this.userInfoReadyCallback(res)
+      //           }
+      //         }
+      //       })
+      //     }
+      //   },
+      //   fail:res =>{
+      //     console.log('-------授权失败')
+      //   }
+      // })
 
-    let that = this;
+      let that = this;
     wx.getSetting({
       success(res) {
         if (!res.authSetting['scope.record']) {
@@ -62,7 +62,6 @@ App({
             scope: 'scope.record',
             success() {
               // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
-              wx.startRecord()
             }
           })
         }
