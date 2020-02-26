bc3871c12ddc9221960513f19f3de05aa4c506b1
huangtao <huangtao>
2020-02-26 17:09:00 +0800

    新增公共标题栏组件

---
+++ pages/index/service-index/ht/module-title/module-title.js
@@ -0,0 +1,51 @@
+// pages/index/service-index/ht/module-title/module-title.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 标题名
+    title: {
+      type: String,
+      value: "标题"
+    },
+    // 字体大小
+    fontSize: {
+      type: String,
+      value: "34rpx"
+    },
+    // 字体颜色
+    color: {
+      type: String,
+      value: "#242526"
+    },
+    // 加粗成都
+    fontWeight: {
+      type: String,
+      value: "bold"
+    },
+    // 是否有数量
+    isHaveNum: {
+      type: Boolean,
+      value: false
+    },
+    num: {
+      type: String,
+      value: ""
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
