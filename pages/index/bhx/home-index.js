c4c3edde90be09a197882693ed4c3aa206e1b7c6
baohexin <1191972217@qq.com>
2020-02-27 19:58:07 +0800

    首页界面布局

---
+++ pages/index/bhx/home-index.js
@@ -0,0 +1,77 @@
+// pages/index/bhx/home-index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 首页banner
+    home_houlipin_hospital_banner:"/images/home/home_houlipin_hospital_banner.png",
+    serviceBg: '/images/home/home_service_bg.png',
+    goodAts: ["风湿骨科", "针灸"],
+    isShowAllContent: false, 
+    evaluateData: [], // 院长介绍
+    deanContent: "太原侯丽萍风湿骨病中医医院院长，北京中医药大学博士生导师，山西省名中医，国家中医药管理局重点专科…",
+    hospitalContent: "太原侯丽萍风湿骨病中医医院系国家中医药管理局“十五”、“十一五”、“十二五”风湿病重点专科医院、太原市二级甲等中医专科医院、山西中医学院教学医院、山西中医类风…",
+  },
+
+  attached: function () {
+    let that = this;
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
