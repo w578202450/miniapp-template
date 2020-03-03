72607cfaa34c57e034b24cc218b86198033fb983
fu zhimin <15184467065@163.com>
2020-03-03 16:45:51 +0800

    no message

--- pages/index/service-index/service-index.js
+++ pages/index/service-index/service-index.js
@@ -95,7 +95,11 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    if (this.data.isSearchState) {
+      this.initDocInfoFun();
+      this.patientShareGet();
+      this.inquiryCaseGet();
+    }
   },
 
   /**
