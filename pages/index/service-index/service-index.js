f3f3aac689065f99afc4e01d15d186051cdcd2ad
baohexin <1191972217@qq.com>
2020-04-03 15:49:30 +0800

    专家门诊底部弹出框

--- pages/index/service-index/service-index.js
+++ pages/index/service-index/service-index.js
@@ -67,7 +67,9 @@ Page({
         orgID: "",
         doctorStaffID: ""
       }
-    }
+    },
+    hideModal: true, //模态框的状态  true-隐藏  false-显示
+    animationData: {},
   },
 
   /**
@@ -424,6 +426,56 @@ Page({
       let nextPageName = "chat";
       this.popup.showPopup(nextPageName); // 显示登录确认框
     }
+  },
+
+  /** 资格证书底部弹出浪框 */
+  // 显示对话框
+  showBottomDialog: function() {
+    var that = this;
+    that.setData({
+      hideModal: false
+    });
+     // 显示遮罩层
+    var animation = wx.createAnimation({
+      duration: 100, //动画的持续时间 默认600ms   数值越大，动画越慢   数值越小，动画越快
+      timingFunction: 'ease', //动画的效果 默认值是linear
+      delay: 0
+    })
+    this.animation = animation;
+    setTimeout(function() {
+      that.fadeIn(); //调用显示动画
+    }, 200)
+  },
+
+ // 隐藏对话框
+  hideModal: function() {
+    var that = this;
+    // 隐藏遮罩层
+    var animation = wx.createAnimation({
+      duration: 100, //动画的持续时间 默认800ms   数值越大，动画越慢   数值越小，动画越快
+      timingFunction: 'ease', //动画的效果 默认值是linear
+    })
+    this.animation = animation;
+    that.fadeDown(); //调用隐藏动画   
+    setTimeout(function() {
+      that.setData({
+        hideModal: true
+      })
+    }, 200) //先执行下滑动画，再隐藏模块
+  },
+
+  //动画集
+  fadeIn: function() {
+    this.animation.translateY(0).step()
+    this.setData({
+      animationData: this.animation.export() //动画实例的export方法导出动画数据传递给组件的animation属性
+    })
+  },
+  fadeDown: function() {
+    this.animation.translateY(500).step()
+    this.setData({
+      animationData: this.animation.export(),
+    })
   },
 
   /**取消事件 */
