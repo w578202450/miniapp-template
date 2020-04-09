224c07ffa52647911bd8ff5991693f4df217da70
fu zhimin <15184467065@163.com>
2020-04-09 13:30:52 +0800

    no message

---
+++ components/tmc-noData/tmc-noData.js
@@ -0,0 +1,31 @@
+// components/tmc-noData/tmc-noData.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    text:{
+      type:String,
+      value:"暂无数据"
+    },
+    // 是否隐藏头部分割线
+    hiddenTop:{
+      type:Boolean,
+      value:true
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
