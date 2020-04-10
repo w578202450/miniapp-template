a58657cbc230e926b0a5ef9ae75f5acf2e615ed3
baohexin <1191972217@qq.com>
2020-04-10 17:08:46 +0800

    修改跑马灯效果

--- pages/index/home-index/home-dazhong-page/home-dazhong-page.js
+++ pages/index/home-index/home-dazhong-page/home-dazhong-page.js
@@ -12,6 +12,7 @@ Component({
       type: String,
       value: ""
     },
+    onload: Boolean
   },
 
   /**
@@ -26,15 +27,101 @@ Component({
     helpfulNum: 12324,
     medicineIcon: "/images/home/home_dazhong_medicine.png",
     rectangleBackgroundImg: "/images/home/home_rectangle_background.png",
-    news: [
-      '李**     已获得免费持续治疗援助1支       刚刚',
-      '苏**     已获得免费持续治疗援助2支       1分钟前',
-      '张**     已获得免费援助药品一个月用量    3分钟前',
-      '王**     已获得免费持续治疗援助2支       6分钟前',
-      '程**     已获得免费持续治疗援助3支       8分钟前',
-      '吴**     已获得免费援助药品二个月用量    10分钟前',
-
+    // news: [
+    //   '李**     已获得免费持续治疗援助1支       刚刚',
+    //   '苏**     已获得免费持续治疗援助2支       1分钟前',
+    //   '张**     已获得免费援助药品一个月用量    3分钟前',
+    //   '王**     已获得免费持续治疗援助2支       6分钟前',
+    //   '程**     已获得免费持续治疗援助3支       8分钟前',
+    //   '吴**     已获得免费援助药品二个月用量    10分钟前',
+    // ],
+    patientList: [
+      {
+        keyID: "1",
+        patientName: "李**",
+        content: "已获得免费持续治疗援助1支",
+        time: "刚刚"
+      },
+      {
+        keyID: "2",
+        patientName: "苏**",
+        content: "已获得免费持续治疗援助2支",
+        time: "1分钟前"
+      },
+      {
+        keyID: "3",
+        patientName: "刘**",
+        content: "已获得免费援助药品1个月用量",
+        time: "3分钟前"
+      },
+      {
+        keyID: "4",
+        patientName: "蒋**",
+        content: "已获得免费持续治疗援助1支",
+        time: " 8分钟前"
+      },
+      {
+        keyID: "5",
+        patientName: "王**",
+        content: "已获得免费持续治疗援助2支",
+        time: "15分钟前"
+      },
+      {
+        keyID: "6",
+        patientName: "李**",
+        content: "已获得免费援助药品1个月用量",
+        time: "19分钟前"
+      },
+      {
+        keyID: "7",
+        patientName: "陈**",
+        content: "已获得免费持续治疗援助2支",
+        time: "20分钟前"
+      },
+      {
+        keyID: "8",
+        patientName: "曾**",
+        content: "已获得免费持续治疗援助1支",
+        time: "27分钟前"
+      },
+      {
+        keyID: "9",
+        patientName: "赖**",
+        content: "已获得免费持续治疗援助2支",
+        time: "半小时前"
+      },
+      {
+        keyID: "10",
+        patientName: "邱**",
+        content: "已获得免费持续治疗援助2支",
+        time: "半小时前"
+      },
+      {
+        keyID: "11",
+        patientName: "邢**",
+        content: "已获得免费援助药品1个月用量",
+        time: "半小时前"
+      },
+      {
+        keyID: "12",
+        patientName: "徐**",
+        content: "已获得免费持续治疗援助2支",
+        time: "半小时前"
+      },
+      {
+        keyID: "13",
+        patientName: "贾**",
+        content: "已获得免费持续治疗援助1支",
+        time: "1小时前"
+      },
+      {
+        keyID: "14",
+        patientName: "汪**",
+        content: "已获得免费持续治疗援助2支",
+        time: "1小时前"
+      }
     ],
+
     autoplay: true,
     interval: 1000,
     duration: 1500,
@@ -48,12 +135,20 @@ Component({
     toServiceIndexFun() {
       this.triggerEvent('toServiceIndexFun');
     },
-    createHelpfulNumFun() {
+    createHelpfulNumFun: function() {
+      let that = this;
       let date1 = "1970-01-01 00:00:00"; // 开始时间
       let date2 = new Date(); // 结束时间
       let date3 = date2.getTime() - new Date(date1).getTime(); // 时间差的毫秒数
-      this.helpfulNum = Math.round((date3 / 1000 - (3600 * 24 * 365 * 49.6)) / 1800); },
+      let randomNum = Math.round((date3 / 1000 - (3600 * 24 * 365 * 49.6)) / 1800); /** 一小时加两个 */
+      that.setData({
+        helpfulNum: (int)(Math.random() * 100)
+      });
+      console.log("========helpfulNum=======" + that.data.helpfulNum);
+    },
+  },
 
+  observers: {
 
   }
 })
