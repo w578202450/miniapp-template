2e67f14a0276dbec9d7cf34d948ba501f41e3cc7
baohexin <1191972217@qq.com>
2020-04-10 19:02:47 +0800

    大冢页面完善修改

--- pages/index/home-index/home-dazhong-page/home-dazhong-page.js
+++ pages/index/home-index/home-dazhong-page/home-dazhong-page.js
@@ -12,7 +12,13 @@ Component({
       type: String,
       value: ""
     },
-    onload: Boolean
+    onload:{
+      type: Boolean,
+      value: false,
+      observer: function (newVal, oldVal) {
+        this.createHelpfulNumFun()
+      }
+    } 
   },
 
   /**
@@ -26,6 +32,7 @@ Component({
     middleOnefei4: "https://com-shuibei-peach-static.100cbc.com/tmccontent/6788/org/fei4.png",
     helpfulNum: 12324,
     medicineIcon: "/images/home/home_dazhong_medicine.png",
+    bottomIcon: "/images/home/home_dazhong_bottom_icon.png",
     rectangleBackgroundImg: "/images/home/home_rectangle_background.png",
     // news: [
     //   '李**     已获得免费持续治疗援助1支       刚刚',
@@ -137,18 +144,16 @@ Component({
     },
     createHelpfulNumFun: function() {
       let that = this;
-      let date1 = "1970-01-01 00:00:00"; // 开始时间
+      let date1 = new Date("1970-01-01 00:00:00"); // 开始时间
       let date2 = new Date(); // 结束时间
-      let date3 = date2.getTime() - new Date(date1).getTime(); // 时间差的毫秒数
+      console.log(date1.getTime());
+      console.log(date2.getTime());
+      let date3 = date2.getTime() - date1.getTime(); // 时间差的毫秒数
       let randomNum = Math.round((date3 / 1000 - (3600 * 24 * 365 * 49.6)) / 1800); /** 一小时加两个 */
       that.setData({
-        helpfulNum: (int)(Math.random() * 100)
+        helpfulNum: randomNum
       });
-      console.log("========helpfulNum=======" + that.data.helpfulNum);
     },
   },
 
-  observers: {
-
-  }
 })
